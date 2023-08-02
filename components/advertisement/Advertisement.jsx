a146b4d97855cd87c400211fb4f668d1b5733d38
Joginder Kumar <joginder.director@gmail.com>
2023-08-02 11:15:25 +0530

    Update Advertisement.jsx

--- components/advertisement/Advertisement.jsx
+++ components/advertisement/Advertisement.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import {Adsense} from '@ctrl/react-adsense';
+import {Adsense} from '@ctrl/react-adsense';
 
 const Advertisement = () => {
  
@@ -7,14 +7,14 @@ const Advertisement = () => {
     <>
       <section className="google-add pt-4"  >
         <div className="container">
-          {/* <Adsense
+          <Adsense
             className="adsbygoogle"
             style={{ display: "block" }}
             data-ad-client="ca-pub-6596297344025689"
             data-ad-slot="1458448211"
             data-ad-format="auto"
             data-full-width-responsive="true"
-          /> */}
+          />
         </div>
       </section>
     </>
