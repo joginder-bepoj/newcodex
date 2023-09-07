95549f168dea195ec99826ffc651c83df76b625b
Joginder Kumar <joginder.director@gmail.com>
2023-09-07 17:50:34 +0530

    Update Advertisement.jsx

--- components/advertisement/Advertisement.jsx
+++ components/advertisement/Advertisement.jsx
@@ -1,30 +1,30 @@
-import React, {useEffect} from "react";
-// import {Adsense} from '@ctrl/react-adsense';
+import React from "react";
+import {Adsense} from '@ctrl/react-adsense';
 // import { useRouter } from "next/router";
 
 const Advertisement = () => {
-  useEffect(() => {
-    if (window) {
-      (window.adsbygoogle = window.adsbygoogle || []).push({});
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (window) {
+  //     (window.adsbygoogle = window.adsbygoogle || []).push({});
+  //   }
+  // }, []);
  
   return (
     <>
       <section className="google-add pt-4"  >
         <div className="container">
-         <ins className="adsbygoogle"
+{/*          <ins className="adsbygoogle"
         style={{display: "block"}}
         data-ad-client="ca-pub-6596297344025689"
-        data-ad-slot="1458448211"></ins>
-{/*           <Adsense
+        data-ad-slot="1458448211"></ins> */}
+          <Adsense
             className="adsbygoogle"
             style={{ display: "block" }}
             data-ad-client="ca-pub-6596297344025689"
             data-ad-slot="1458448211"
             data-ad-format="auto"
             data-full-width-responsive="true"
-          /> */}
+          />
         </div>
       </section>
     </>
